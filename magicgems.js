50e7fa0148e07d5688d69e03809e4cd7bd8b2997
Baka9k <neoplanspaceliner9@gmail.com>
2015-09-29 12:54:01 +0300

    Fixed magicgems.click(), updated magicgems.draw()

--- magicgems.js
+++ magicgems.js
@@ -23,7 +23,6 @@ magicgems.createGameField = function(element) {
 	magicgems.generateMap();
 	magicgems.gamefield.canvas.addEventListener('click', magicgems.click, false);
 	magicgems.draw();
-	
 	console.log("Game field created in " + element.id + " element.");
 }
 
@@ -54,15 +53,6 @@ magicgems.gems = {
 		},
 }
 
-magicgems.inArray = function(arr,val) {
-	for (var i = 0; i < arr.length; i++)	{
-		if (arr[i] == val) {
-			return true;
-		}
-	}
-	return false;
-}
-
 magicgems.generateTile = function() {
 	var probability = Math.round(magicgems.rand(0,3));
 	switch(probability) {
@@ -86,16 +76,19 @@ magicgems.generateMap = function() {
 	for (var i = 0; i < magicgems.map.length; i++) {
 		for (var j = 0; j < magicgems.map[i].length; j++) {
 			var tile = magicgems.generateTile();
-			tile.x = i;
-			tile.y = j;
+			tile.x = j;
+			tile.y = i;
 			magicgems.map[i][j] = tile;
 		}
 	}
 }
 
 magicgems.draw = function() {
+	magicgems.gamefield.context.fillStyle = "#000000";
+	magicgems.gamefield.context.fillRect(0,0,magicgems.gamefield.width,magicgems.gamefield.height);
 	for (var i = 0; i < magicgems.map.length; i++) {
 		for (var j = 0; j < magicgems.map[i].length; j++) {
+			if (magicgems.map[i][j] == "void") continue;
 			magicgems.gamefield.context.fillStyle = magicgems.map[i][j].color;
 			magicgems.gamefield.context.fillRect(j * magicgems.tileWidth, i * magicgems.tileHeight, magicgems.tileWidth, magicgems.tileHeight);
 		}
@@ -112,6 +105,7 @@ magicgems.click = function(e) {
 	var tileX = Math.floor(x / magicgems.tileWidth);
 	var tileY = Math.floor(y / magicgems.tileHeight);
 	var checked = {};
+	checked[tileX + "," + tileY] = true;
 	
 	var clickedGem = magicgems.map[tileY][tileX];
 	
@@ -123,15 +117,11 @@ magicgems.click = function(e) {
 		for (var i = 0; i < neighborsCoords.length; i++) {
 			var x = neighborsCoords[i].x;
 			var y = neighborsCoords[i].y;
-			
-			//if ((typeof map[neighborsCoords[i].y] != "undefined") && (typeof map[neighborsCoords[i].y][neighborsCoords[i].x] != "undefined") && (map[neighborsCoords[i].y][neighborsCoords[i].x].type == map[tileY][tileX].type))  {
-			
 			if (map[y][x].type == clickedGem.type)  {
 				neighbors.push(map[y][x]);
 			}
 		}
 		
-		console.log(neighbors);
 		if (neighbors.length == 0) return [];
 		
 		for (var i = 0; i < neighbors.length; i++) {
@@ -142,272 +132,16 @@ magicgems.click = function(e) {
 				neighbors = neighbors.concat(arr);
 			}
 		}
-		if (map[tileY][tileX].type == clickedGem.type) {
-			return neighbors.concat([map[tileY][tileX]]);
-		} else {
-			return neighbors;
-		}
+		return neighbors;
 	}
 	
 	var group = checkNeighbors(tileX,tileY);
+	for (var i = 0; i < group.length; i++) {
+		magicgems.map[group[i].y][group[i].x] = "void";
+	}
+	magicgems.draw();
+	console.log(magicgems.map);
 	
-	console.log(group);
 }
 
 
-
-
-
-
-
-
-/*
-	ad.tileHeight = 40;
-	ad.tileWidth = 40;
-	ad.tilesOnX = Math.ceil(ad.width/ad.tileWidth);
-	ad.tilesOnY = Math.ceil(ad.height/ad.tileHeight);
-	ad.lr = 'r';
-	ad.o2 = 1;
-	ad.keys = 'enabled';
-	ad.map = new Array();
-	ad.currentMap = new Array();
-	ad.displacementX = 0;
-	ad.displacementY = 0;
-	ad.camera = {};
-	ad.currentFromX = 0;
-	ad.currentFromY = 0;
-	ad.currentDisplacementX = 0;
-	ad.currentDisplacementY = 0;
-	ad.items = {
-		quartz: 0,
-		emerald: 0,
-		ruby: 0,
-		diamond: 0
-	};
-	ad.context.font = '16px lucida console';
-	ad.characteristics = {
-		o2consumption: 0.00010
-	};
-	ad.dx = 0;
-	ad.dy = 0;
-	
-	Array.prototype.in_array = function(p_val) {
-		for(var i = 0, l = this.length; i < l; i++)	{
-			if(this[i] == p_val) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	ad.draw = function(img, x, y, width, height) {
-		ad.context.drawImage(img, Math.round(x), Math.round(y), width, height);
-	}
-
-	ad.drawBoat = function(x, y, width, height) {
-		if (ad.lr == 'l') {
-			var img = document.getElementById("boat-l");             
-			ad.context.drawImage(img, x, y, width, height);
-		}
-		if (ad.lr == 'r') {
-			var img = document.getElementById("boat-r");
-			ad.context.drawImage(img, x, y, width, height);
-		}
-	}
-
-	ad.coordHash = function(a, b) {
-		var hash = a ^ b;
-		for(var i = 0; i < 3; i++) {
-			hash = hash*31 ^ (a << (0x0F & hash-1)) ^ (b << (0x0F & hash-31));
-		}
-		return hash;            
-	}
-
-	ad.hash2prob = function(hash) {
-		return (hash & 0xFFFF)/65536;
-	}
-
-	ad.drawTile = function(tileX, tileY, dispX, dispY) {
-		var x = (tileX - ad.currentDisplacementX)* ad.tileWidth + dispX;
-		var y = (tileY - ad.currentDisplacementY)* ad.tileHeight + dispY;
-		var xandy = (tileX + 'and' + tileY);
-		var ap = ad.hash2prob(ad.coordHash(tileX, tileY));
-		
-		//Sky
-		if (tileY < 6){
-			ad.context.fillStyle = "#87CEFA";
-			ad.context.fillRect(x, y, ad.tileWidth, ad.tileHeight);
-		} 
-
-		//Ground
-		if (tileY == 21) {
-			var probability = 0.5;
-			if ((ap < probability) && (!ad.map.in_array(xandy))) {
-				ad.draw(stone, x, y, ad.tileWidth, ad.tileHeight);
-			}
-		}
-		if ((tileY > 21) && (ad.map.in_array(xandy) == false)) {
-			ad.draw(stone, x, y, ad.tileWidth, ad.tileHeight);
-		}
-		var probability0 = 0.01;
-		if ((tileY > 30) && (ap < probability0) && (ad.map.in_array(xandy) == false)) {
-			ad.draw(quartz, x, y, ad.tileWidth, ad.tileHeight);
-		}
-		probability0 = 0.01;
-		var probability1 = 0.02;
-		if ((tileY > 71) && (tileY < 170) && (ap > probability0) && (ap < probability1) && (ad.map.in_array(xandy)==false)) {
-			ad.draw(emerald, x, y, ad.tileWidth, ad.tileHeight);  
-		}
-	}
-	
-	ad.keypress = function(event) {
-		if (ad.keys == 'enabled') {
-			k = event.keyCode;
-			switch(k) {
-				case 38:
-					ad.camera.up();
-					break;
-				case 40:
-					ad.camera.down();
-					break;
-				case 37:
-					ad.camera.left();
-					break;
-				case 39:
-					ad.camera.right();
-					break;
-			}
-		}
-		//<	   37
-		//^	   38
-		//>    39
-		//v    40
-	}
-	
-	document.addEventListener('keydown', ad.keypress, false);
-
-	ad.dig = function (tileX, tileY) {
-		var xandy = (tileX + 'and' + tileY);
-		var ap = ad.hash2prob(ad.coordHash(tileX, tileY));
-		var probability = 0.01;
-		if ((tileY > 30) && (tileY < 70) && (ap < probability) && (!ad.map.in_array(xandy))) {
-			ad.items.quartz++;
-		}
-		probability = 0.01;
-		if ((tileY > 71) && (tileY < 170) && (ap < probability) && (!ad.map.in_array(xandy))) {
-			ad.items.emerald++;
-		}
-		if(!ad.map.in_array(xandy)) {
-			ad.map.push(xandy);
-		}
-	}
-	
-	ad.camera.right = function () {
-		ad.lr = 'r';
-		ad.keys = "disabled";
-		ad.displacementX++;
-		ad.dig(Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
-		ad.drawWorld(ad.displacementX, ad.displacementY);
-	}
-
-	ad.camera.left = function () {
-		ad.lr = 'l';
-		ad.keys = "disabled";
-		ad.displacementX--;
-		ad.dig(Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
-		ad.drawWorld(ad.displacementX, ad.displacementY);
-	}
-
-	ad.camera.down = function () {
-		ad.keys = "disabled";
-		ad.displacementY++;
-		ad.dig(Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
-		ad.drawWorld(ad.displacementX, ad.displacementY);
-	}
-	
-	ad.camera.up = function () {
-		if (ad.displacementY <= 0) return;
-		ad.keys = "disabled";
-		ad.displacementY--;
-		ad.dig(Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
-		ad.drawWorld(ad.displacementX, ad.displacementY);
-	}
-
-	ad.drawWorld = function (fromX, fromY) {
-		var xandy = ((Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1) + 'and' + (6 + ad.displacementY));
-		var ap = ad.hash2prob(ad.coordHash((Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1), (6 + ad.displacementY)));
-		if (((!ad.currentMap.in_array(xandy)) && ((fromY + 6) > 21)) || ((!ad.currentMap.in_array(xandy)) && ((fromY + 6) == 21) && (ap < 0.5))) {
-			var t = 70;
-		} else {
-			var t = 20;
-		}
-		var stepX = Math.round((ad.currentFromX - fromX) * ad.tileWidth / t * 100) / 100;
-		var stepY = Math.round((ad.currentFromY - fromY) * ad.tileHeight / t * 100) / 100;
-		var nStepsX = (ad.currentFromX - fromX) * ad.tileWidth / stepX;
-		var nStepsY = (ad.currentFromY - fromY) * ad.tileHeight / stepY;
-		var c = 0;
-		
-		var step = function() {
-			ad.raf = window.requestAnimationFrame(step);
-			ad.dx += stepX;
-			ad.dy += stepY;
-			c++;
-			ad.context.fillStyle = "#6495ED";
-			ad.context.fillRect(0, 0, ad.width, ad.height); //Background (water)
-			for (var i = ad.currentFromX - 1; i <= ad.currentFromX + ad.tilesOnX; i++) {
-				for (var j = ad.currentFromY - 1; j <= ad.currentFromY + ad.tilesOnY; j++) {
-					ad.drawTile(i, j, ad.dx, ad.dy);
-				}
-			}
-			ad.drawBoat((Math.ceil(ad.tilesOnX / 2) * ad.tileWidth) - ad.tileWidth + 1, 6 * (ad.tileHeight) + ad.tileHeight * 0.2, ad.tileWidth - 2, ad.tileHeight * 0.725);
-			ad.drawTools();
-			ad.o2 -= ad.characteristics.o2consumption;
-			if ((c > nStepsX) || (c > nStepsY)) {
-				cancelAnimationFrame(ad.raf);
-				ad.dx = 0;
-				ad.dy = 0;
-				ad.currentDisplacementX = ad.displacementX;
-				ad.currentDisplacementY = ad.displacementY;
-				ad.currentMap = [];
-				ad.currentMap = ad.map.concat(ad.currentMap);
-				ad.keys = "enabled";
-			}
-			if (ad.o2 <= 0) {
-				ad.o2 = 0;
-				ad.keys = "disabled";
-				ad.context.font = '56px lucida console';
-				ad.context.fillStyle = "#111111";
-				ad.context.fillText('GAME OVER', ad.width / 2 - 200, ad.height / 2 - 60);
-			}
-		}
-		
-		step();
-		ad.currentFromX = fromX;
-		ad.currentFromY = fromY;
-		if (ad.displacementY == 0) {
-			ad.o2 = 1;
-		}
-	}
-	
-	ad.drawTools = function() {
-		ad.context.font = '16px lucida console';
-		ad.context.fillStyle = "#DDDDDD";
-		ad.context.fillRect(8, 8, 80, 160);
-		ad.draw(quartz, 36, 12, 20, 20);
-		ad.context.fillStyle = "#555555";
-		ad.context.fillText(ad.items.quartz, 65, 28);
-		ad.draw(emerald, 36, 42, 20, 20);
-		ad.context.fillText(ad.items.emerald, 65, 58);
-		ad.draw(ruby, 36, 72, 20, 20);
-		ad.context.fillText(ad.items.ruby, 65, 88);
-		ad.draw(diamond, 36, 102, 20, 20);
-		ad.context.fillText(ad.items.diamond, 65, 118);
-		ad.context.fillStyle = "#00DD00";
-		ad.context.fillRect(15, 15, 10, 130 * ad.o2);
-		ad.context.fillText('O2', 15, 160);
-	}
-	
-	
-	ad.drawWorld(0,0);
-}
-*/
